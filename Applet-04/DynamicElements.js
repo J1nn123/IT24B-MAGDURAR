39f779552ec7e478cafe4828c20a30e65d8d0039
Magdura Roland <jinj11339@gmail.com>
2024-10-13 23:54:40 +0800

    Applet-04 continued

--- Applet-04/DynamicElements.js
+++ Applet-04/DynamicElements.js
@@ -1,25 +1,42 @@
-fetch("DynamicElement.json")
 
-.then (function (response){
-    return response.json();
-})
-
-
-
-.then (function(Dynamicelements){
-let placeholder = document.query.querySelector ("#data-output");
-let out = "";
-for(let Dynamicelement of Dynamicelements ){
-out += `
-<button>
-${Dynamicelement.name}
-
-<button>
-
-`;
-}
-
-
-
-})
+$('#search-input').on('keyup', function(){
+    var value = $(this).val()
+    console.log ('Value:', value)
+    var data = searchTable(value, myArray)
+    buildTable(data)
+    })
     
+    buildTable(myArray)
+    
+    function searchTable(value, data){
+    var filteredData=[]
+    for (var i = 0; i<data.length ; i++){
+      value =value.toLowerCase()
+    var name =data[i].name.toLowerCase();
+    
+      if (name.includes(value)){
+    filteredData.push(data[i])
+    
+      }
+    }
+    return filteredData
+    }
+    
+    
+       
+     
+        
+    function buildTable(data){
+        var table = document.getElementById('Table')
+        table.innerHTML = ''
+        for (var i = 0; i < data.length; i++){
+            
+            var row = `<tr>
+                            <td>${data[i].name}</td>
+                            <td>${data[i].age}</td>
+                            <td>${data[i].birthdate}</td>
+                       </tr>`
+            table.innerHTML += row
+        }
+    }
+    document.getElementById("Dynamic");
