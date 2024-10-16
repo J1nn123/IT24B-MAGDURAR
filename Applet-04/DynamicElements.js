af94afe7147569349c0ffff0be4be98b74107985
Magdura Roland <jinj11339@gmail.com>
2024-10-16 10:59:57 +0800

    Applet-4 done!

--- Applet-04/DynamicElements.js
+++ Applet-04/DynamicElements.js
@@ -1,42 +1,47 @@
+fetch('DynamicElement.json')
+  .then(response => response.json())
+  .then(data => {
+   
+    buildTable(data);
 
-$('#search-input').on('keyup', function(){
-    var value = $(this).val()
-    console.log ('Value:', value)
-    var data = searchTable(value, myArray)
-    buildTable(data)
-    })
     
-    buildTable(myArray)
-    
-    function searchTable(value, data){
-    var filteredData=[]
-    for (var i = 0; i<data.length ; i++){
-      value =value.toLowerCase()
-    var name =data[i].name.toLowerCase();
-    
-      if (name.includes(value)){
-    filteredData.push(data[i])
-    
-      }
+    $('#search-input').on('keyup', function() {
+      var value = $(this).val();
+      console.log('Value:', value);
+      var filteredData = searchTable(value, data);
+      buildTable(filteredData);
+    });
+  })
+  .catch(error => console.error('Error fetching data:', error));
+
+
+function searchTable(value, data) {
+  var filteredData = [];
+  value = value.toLowerCase();
+
+  for (var i = 0; i < data.length; i++) {
+    var name = data[i].name.toLowerCase();
+
+    if (name.includes(value)) {
+      filteredData.push(data[i]);
     }
-    return filteredData
-    }
-    
-    
-       
-     
-        
-    function buildTable(data){
-        var table = document.getElementById('Table')
-        table.innerHTML = ''
-        for (var i = 0; i < data.length; i++){
-            
-            var row = `<tr>
-                            <td>${data[i].name}</td>
-                            <td>${data[i].age}</td>
-                            <td>${data[i].birthdate}</td>
-                       </tr>`
-            table.innerHTML += row
-        }
-    }
-    document.getElementById("Dynamic");
+  }
+
+  return filteredData;
+}
+
+
+function buildTable(data) {
+  var table = document.getElementById('Table');
+  table.innerHTML = ''; 
+
+  for (var i = 0; i < data.length; i++) {
+    var row = `<tr>
+                  <td>${data[i].name}</td>
+                  <td>${data[i].age}</td>
+                  <td>${data[i].Course}</td>
+                  <td>${data[i].Address}</td>
+               </tr>`;
+    table.innerHTML += row;
+  }
+}
